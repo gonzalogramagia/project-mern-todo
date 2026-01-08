bce2c5b8d1c145ccd7f40efb3efbafced598b2e4
Gonza <gonzalogramagia@gmail.com>
2026-01-07 23:56:53 -0300

    feat: set default dark mode and improve spinner styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </ChakraProvider>
     </BrowserRouter>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -81,7 +81,15 @@ const HomePage = () => {
           📌 Current Tasks
         </Text>
 
-        {loading && <Spinner size={"xl"} />}
+        {loading && (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        )}
 
         {!loading && (
           <SimpleGrid
